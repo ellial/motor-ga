670c7dfcbc88c3668efc01e5528f6094504e511e
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2021-04-22 02:28:27 +0300

    my part on class methods

--- gen_alg_main.m
+++ gen_alg_main.m
@@ -1,21 +1,26 @@
+addpath('c:\\femm42\\mfiles'); % linux (not working) => '/home/c/.wine/drive_c/femm42/mfiles'
+savepath; % sudo chmod 666 /usr/local/MATLAB/R2020b/toolbox/local/pathdef.m
+openfemm(2)
+
 pop_size = 10;
 const_state_ratio = 2/3;
-population = zeros(pop_size);
+population = zeros(1,pop_size);
 for i = 1:pop_size
-    population(i) = myMotor(randomMotorinit);
+    population(i) = myMotor(); % no params means random init
 end
-while NOT terminated
+while ~terminated()
    while new_ch < population * const_state_ratio
       sort(population); %according to fitness
       parent_1 = population(1);
       parent_2 = population(2);
-      (child_1,child_2) = crossover(parent_1,parent_2);
+      ll = crossover(parent_1, parent_2);
+      child_1 = ll(1);
+      child_2 = ll(2);
       
-      child1.mutate();
-      child1.eval_fitness();
-      child2.mutate();
-      child2.eval_fitness();
-      
+      child_1.mutate();
+      child_2.mutate();
+      child_1.eval_fitness();
+      child_2.eval_fitness();
       
       %++check fitness
       population(pop_size) = child_1;
--- myMotor.m
+++ myMotor.m
@@ -3,58 +3,134 @@ classdef myMotor
         rso     {mustBeNumeric}
         rsi     {mustBeNumeric}
         dm      {mustBeNumeric} 	% Magnet thickness 
-        dc      {mustBeNumeric}    % Can thickness 
+        dc      {mustBeNumeric}     % Can thickness 
         ds      {mustBeNumeric}     % depth of slot opening 
         fm      {mustBeNumeric}     % Pole fraction spanned by the magnet 
-        fp 		{mustBeNumeric}% Pole fraction spanned by the iron 
-        ft 		{mustBeNumeric}% Width of tooth as a fraction of pole pitch at stator ID 
-        fb  	{mustBeNumeric}	% Back iron thickness as a fraction of tooth thickness 
-        go  	{mustBeNumeric}	% stator to magnet mechanical clearance 
-        hh  	{mustBeNumeric}	% length in the into-the-page direction 
+        fp 		{mustBeNumeric}     % Pole fraction spanned by the iron 
+        ft 		{mustBeNumeric}     % Width of tooth as a fraction of pole pitch at stator ID 
+        fb  	{mustBeNumeric}     % Back iron thickness as a fraction of tooth thickness 
+        go  	{mustBeNumeric}     % stator to magnet mechanical clearance 
+        hh  	{mustBeNumeric}     % length in the into-the-page direction 
         Jpk  	{mustBeNumeric}
         fitness {mustBeNumeric}
         mass    {mustBeNumeric}
-    
     end
     
     
     methods
     
         function obj = myMotor(rso,rsi,dm,dc,ds,fm,fp,ft,fb,go,hh,Jpk) % constructor
-            
+            if nargin == 0 % random initialization - CHECK RANGE WITH GKITS
+                           % wtf is newrso on original gkitscode  
+                % try random initialization until you meet the constraints
+                while true 
+                    obj.rso = rand();
+                    obj.rsi = rand();
+                    obj.dm  = rand();
+                    obj.dc  = rand();
+                    obj.ds  = rand();
+                    obj.fm  = rand();
+                    obj.fp  = rand();
+                    obj.ft  = rand();
+                    obj.fb  = rand();
+                    obj.go  = rand();
+                    obj.hh  = rand();
+                    obj.Jpk = rand();
+                    
+                    if (obj.check_constraints()) 
+                        break;
+                    end
+                end
+            else % initialize on params
+                obj.rso = rso;
+                obj.rsi = rsi;
+                obj.dm  = dm;
+                obj.dc  = dc;
+                obj.ds  = ds;
+                obj.fm  = fm;
+                obj.fp  = fp;
+                obj.ft  = ft;
+                obj.fb  = fb;
+                obj.go  = go;
+                obj.hh  = hh;
+                obj.Jpk = Jpk;              
+            end
         end
-        function mutated = mutate(p_m)
-           while not check_constraints()
-               %do stuff
-           end
+        
+%         function mutated = mutate(self, p_m)
+%            while not check_constraints()
+%                %do stuff
+%            end
+%         end
+        
+        function totalMass = compute_mass(self)
+            BuildMotor(self.rso, self.rsi, self.dm, self.dc, self.ds, ...
+                self.fm, self.fp, self.ft, self.fb, self.go, self.hh, self.Jpk);
+            mi_saveas('temp.fem');
+            mi_analyze(1);
+            mi_loadsolution;
+
+            % Compute torque for a fixed length to figure out how long the machine
+            % needs to be to get the desired torque;
+            mo_groupselectblock(1);
+            tq = mo_blockintegral(22);
+            self.hh = max([self.hh*tqdes/abs(tq),30]);
+            %newhh = hh*tqdes/abs(tq);
+            mo_clearblock;
+
+            %Copper Mass
+            mo_groupselectblock(3);
+            Copper_Mass = 0.5*mo_blockintegral(5)*self.hh*8960/1000;
+            mo_clearblock;
+
+            %Iron Mass
+            mo_selectblock((self.rsi+self.rso)/2,0)
+            mo_selectblock(self.rso+self.go+self.dm+self.dc/2,0)
+            Iron_Mass = mo_blockintegral(5)*self.hh*7870/1000;
+            mo_clearblock;
+
+            %Magnet Mass
+            mo_selectblock(self.rso+self.go+self.dm/2,0)
+            Magnet_Mass = 14*mo_blockintegral(5)*self.hh*7650/1000;
+            mo_clearblock;
+
+            %Total Mass
+            totalMass = round(Magnet_Mass+Iron_Mass+Copper_Mass, 2);
         end
-        function bOK = check_constraints()
+        
+        function fit = eval_fitness(self)
+            fit = calc_mass(self);
+        end
+
+    end
+    
+    methods (Access = private)       
+        
+        function bOK = check_constraints(self)
             bOK = 1;
-            if ((newrsi + newds) > newrso) bOK=0; end
-            if (newds < 0) bOK=0; end
-            if (newdc < 0) bOK=0; end
-            if ((newfm > 1) || (newfm<0)) bOK=0; end
-            if ((newfp > 1) || (newfp<0)) bOK=0; end
-            if ((newft > 1) || (newft<0)) bOK=0; end
-         
+            if ((self.rsi + self.ds) > self.rso) 
+                bOK=0; end
+            if (self.ds < 0) 
+                bOK=0; end
+            if (self.dc < 0) 
+                bOK=0; end
+            if ((self.fm > 1) || (self.fm<0)) 
+                bOK=0; end
+            if ((self.fp > 1) || (self.fp<0)) 
+                bOK=0; end
+            if ((self.ft > 1) || (self.ft<0)) 
+                bOK=0; end    
         end
         
-        function obj = random_init()
-        
-        end
-        
-        function fit = eval_fitness()
-            
-        end
     end
     
 end
- 
-function crossover(motor1,motor2,p_c)
-    %crossover ellis
-    pos = random(1,num_of_params);
-    %check constraints
-    %return tuple ch1,ch2
-end
+
+%function ll = crossover(parent1, parent2 ,p_c)
+%    %crossover ellis
+%    pos = random(1,num_of_params);
+%    %check constraints
+%    %return list ch1,ch2
+%end
  
  
